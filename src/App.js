9778ad05f2d42839885bb8027042e3b441235693
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2023-02-03 15:07:17 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,47 +1,36 @@
 import {useEffect,useState} from 'react'
 import './App.css';
-import { Card,Grid,Typography,CardContent} from '@mui/material';
-import {getData} from './Components/Apis/FetchData'
 
-const App= ({getData}) => {
+const App= () => {
 const [data,setData]= useState([])
 
-//useEffect (() => {
- // const fetchData = async () => {
- // const data = await fetch(`https://api.sampleapis.com/beers/ale`)
- // const json = await data.json();
-  //setData(json);
- // }
+useEffect (() => {
+    let URL = `https://api.sampleapis.com/beers/ale`
+  const fetchData = async () => {
+  const data = await fetch(URL)
+  const json = await data.json();
+  setData(json);
+  }
 
-  //const result = fetchData()
-  //.catch(console.error);
-//},[])
-
-useEffect(()=> {
-  let beers = true;
-  getData()
-  .then(items => {
-    if(beers) {
-      setData(items)
-    }
-  })
-  return () => beers = false;
+  const result = fetchData()
+  .catch(console.error);
+  console.log(result)
 },[])
+
+
 
   return (
     <>
+     <h1>Beer Weekend Information</h1>3
+   {data.map((beer)=> 
+   <div>
    
-<Card elevation ={6} sx={{maxWidh:345}} display="flex">
-  <CardContent style = {{backgroundColor:"#F28304"}}>
-    {data.map(items => <Typography>
-      {items.name}
-    </Typography> )}
-    {data.map(items => 
-    <Grid>
-      {items.price}
-   </Grid>)}
-  </CardContent>
-</Card>
+    <img src={beer.image} alt="">
+    </img>
+      <h2>{beer.name}</h2>
+      <h3>{beer.price}</h3>
+   </div>
+    )}
     </>
    
   );

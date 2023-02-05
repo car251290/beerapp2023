ab7359b4e5277bc4c97e6276c1802f51a41df051
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2023-02-05 14:08:08 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import {useEffect,useState} from 'react'
+import Beer from './Components/beerCard/Beer'
 import './App.css';
 
 const App= () => {
-const [data,setData]= useState([])
-
-useEffect (() => {
-    let URL = `https://api.sampleapis.com/beers/ale`
-  const fetchData = async () => {
-  const data = await fetch(URL)
-  const json = await data.json();
-  setData(json);
-  }
-
-  const result = fetchData()
-  .catch(console.error);
-  console.log(result)
-},[])
-
-
 
   return (
     <>
-     <h1>Beer Weekend Information</h1>3
-   {data.map((beer)=> 
-   <div>
-   
-    <img src={beer.image} alt="">
-    </img>
-      <h2>{beer.name}</h2>
-      <h3>{beer.price}</h3>
-   </div>
-    )}
-    </>
-   
+     <h1>Beer Weekend Information</h1>
+     <Beer/>
+    </> 
   );
 }
 

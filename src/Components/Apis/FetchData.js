6f1bb9c5027844064f07ff6b01e3f955aba22d0c
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2023-02-07 11:28:17 -0800

    Add files via upload

--- src/Components/Apis/FetchData.js
+++ src/Components/Apis/FetchData.js
@@ -1,18 +1,24 @@
 import axios from "axios";
 
- const getData = async () => {
+ const getData = async (beerData) => {
+    
+    const response = await fetch(`https://api.sampleapis.com/beers/ale`)
+    const data = await response.json();
+    beerData(data);
+
 
     //URL=`https://api.sampleapis.com/beers/ale`
     
-    try{
-        return fetch(`https://api.sampleapis.com/beers/ale`)
-        .then(data => data.json())
+    //try{
+      //  return await fetch(`https://api.sampleapis.com/beers/ale`)
+        //.then(data => data.json())
+        //.then(res => res.data)
         
-    } catch(err) {
-        console.log(err)
-
-        return err;
-    }
+   // } catch(err) {
+     //   console.log(err)
+//
+  //      return err;
+    //}
  }
 
  export default getData;

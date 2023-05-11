095d95331f4de1cf80347b85b9871e3139b110eb
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2023-05-10 18:38:23 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import {useEffect,useState} from 'react'
-import Beer from './Components/beerCard/Beer'
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Beers from "./Pages/Beers/Beers";
+import Details from "./Pages/Details/Details";
+import CollapsibleExample from "./Pages/CollapseDetails/CollapsibleExample";
 
-const App= () => {
-
+const App = () => {
   return (
-    <>
-     <h1>Beer Weekend Information</h1>
-     <Beer/>
-    </> 
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/beers" element={<Beers />} />
+          <Route path="/beers/:id" element={<Details />} />
+          <Route path="/Collapse/:id" element={<CollapsibleExample/>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
